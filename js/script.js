7f3ebbef33b9132d3a4f442bbde194743b4780a4
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-31 22:47:48 -0500

    ticket master api working, makiing sure the keys folder is hidden

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: "GET",
+    url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=" + api_key,
+    async: true,
+    dataType: "json",
+    success: function (json) {
+        console.log(json);
+        // Parse the response.
+        // Do other things.
+    },
+    error: function (xhr, status, err) {
+        // Handle errors here.
+    }
+});
