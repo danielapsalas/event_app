667f898556681f5a853472765bba2a28071d8a6c
Daniela Salas <danielapiconsalas@gmail.com>
2023-08-01 17:15:59 -0500

    Made a function that uses the ticketmaster API and the browser's Geolocation API to make an AJAX request to the Ticketmaster API and retrieves events based on a users location

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Error occurred retrieving location");
+    }
+}
+
+//Function that takes a users location and updates shows near them
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // You can use latitude and longitude to do whatever you want with the user's location
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + latitude + "," + longitude + "&apikey=" + ticketms_api_key,
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
+            // Parse the response.
+            // Do other things.
+        },
+        error: function (xhr, status, err) {
+            // Handle errors here.
+        }
+    });
+}
+
+
+
+
 $.ajax({
     type: "GET",
     url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=" + ticketms_api_key,
@@ -12,3 +44,4 @@ $.ajax({
         // Handle errors here.
     }
 });
+
